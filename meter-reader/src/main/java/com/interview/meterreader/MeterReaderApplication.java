cfb8cb874c8d9fb5c8a978a89ee0512501b1fdb0
Jasmina <jasmina.eminovski@gmail.com>
2021-04-22 23:54:19 +0200

    Initial commit

---
+++ meter-reader/src/main/java/com/interview/meterreader/MeterReaderApplication.java
@@ -0,0 +1,13 @@
+package com.interview.meterreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeterReaderApplication.class, args);
+	}
+
+}
---
+++ meter-reader/src/test/java/com/interview/meterreader/MeterReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.meterreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeterReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
